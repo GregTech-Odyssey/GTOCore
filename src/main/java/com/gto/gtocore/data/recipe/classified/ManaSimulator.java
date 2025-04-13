7ca2c8ce04d1ed7b09a5794f7660e3c0b3960113
Kovrax <18918735979@163.com>
2025-04-13 19:02:03 +0800

    阿卡纳蔷薇

--- src/main/java/com/gto/gtocore/data/recipe/classified/ManaSimulator.java
+++ src/main/java/com/gto/gtocore/data/recipe/classified/ManaSimulator.java
@@ -1,6 +1,7 @@
 package com.gto.gtocore.data.recipe.classified;
 
-import com.gto.gtocore.common.data.GTORecipeTypes;
+import com.enderio.base.common.init.EIOFluids;
+import com.gto.gtocore.GTOCore;
 import com.gto.gtocore.utils.RegistriesUtils;
 
 import net.minecraft.world.level.material.Fluids;
@@ -21,6 +22,13 @@ public interface ManaSimulator {
                 .duration(900)
                 .EUt(VA[MV])
                 .MANAt(-20 * BUFF_FACTOR)
+                .save();
+
+        MANA_SIMULATOR_RECIPES.recipeBuilder(GTOCore.id("rosa_arcana"))
+                .inputFluids(new FluidStack(EIOFluids.XP_JUICE.get().getSource(), 1000))
+                .EUt(VA[MV])
+                .duration(1000)
+                .MANAt(-50 * BUFF_FACTOR)
                 .save();
     }
 }
