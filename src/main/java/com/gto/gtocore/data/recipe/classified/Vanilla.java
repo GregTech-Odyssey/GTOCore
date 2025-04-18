9fe6af7a61a7d9700d1a45536f31a5e5676ef431
suried <xinxinsuried@gmail.com>
2025-04-18 13:45:58 +0800

    添加了一个割草镰刀，右键草有极高几率掉落小麦种子

--- src/main/java/com/gto/gtocore/data/recipe/classified/Vanilla.java
+++ src/main/java/com/gto/gtocore/data/recipe/classified/Vanilla.java
@@ -922,7 +922,7 @@ interface Vanilla {
                 "CDC",
                 "ABA",
                 'A', new MaterialEntry(TagPrefix.pipeHugeFluid, GTMaterials.Steel), 'B', new MaterialEntry(TagPrefix.plateDense, GTMaterials.Steel), 'C', GTItems.FLUID_REGULATOR_LV.asStack(), 'D', GTMachines.COMPRESSOR[GTValues.LV].asStack());
-        VanillaRecipeHelper.addShapedRecipe(provider, GTOCore.id("grass_harvestor"), GTOItems.GRASS_HARVESTER.asStack(),
+        VanillaRecipeHelper.addShapedRecipe(provider, GTOCore.id("grass_harvester"), GTOItems.GRASS_HARVESTER.asStack(),
                 "AA ",
                 "B A",
                 "B  ",
