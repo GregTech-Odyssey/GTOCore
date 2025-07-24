b3f84ad8e1a4a36be524cfacd3abebc892be0bb1
xinxinsuried <xinxinsuried@gmail.com>
2025-07-25 01:02:08 +0800

    完善无线连接机

--- src/main/java/com/gtocore/common/saved/MEWirelessSavedData.kt
+++ src/main/java/com/gtocore/common/saved/MEWirelessSavedData.kt
@@ -2,14 +2,18 @@ package com.gtocore.common.saved
 
 import com.gtocore.integration.ae.MEWirelessConnectionMachine
 
+import net.minecraft.core.UUIDUtil
 import net.minecraft.nbt.CompoundTag
 import net.minecraft.nbt.ListTag
-import net.minecraft.nbt.StringTag
+import net.minecraft.nbt.NbtOps
 import net.minecraft.world.level.saveddata.SavedData
-import net.minecraftforge.server.ServerLifecycleHooks
 
 import appeng.api.networking.GridHelper
 import appeng.api.networking.IGridConnection
+import com.gtolib.GTOCore
+import com.hepdd.gtmthings.utils.TeamUtil
+import com.mojang.serialization.Codec
+import com.mojang.serialization.codecs.RecordCodecBuilder
 
 import java.util.UUID
 import kotlin.collections.forEach
@@ -17,71 +21,59 @@ import kotlin.collections.forEach
 private const val NBT_GRID = "MEWirelessConnectionGrids"
 
 object MEWirelessSavedData : SavedData() {
-    val test1 = MEWirelessConnectionGrid("Test1",UUID.randomUUID())
-    val test2 = MEWirelessConnectionGrid("Test2",UUID.randomUUID())
-    val test3 = MEWirelessConnectionGrid("Test3",UUID.randomUUID())
-    val test4 = MEWirelessConnectionGrid("Test4",UUID.randomUUID())
     val MEWirelessGrids = mutableListOf<MEWirelessConnectionGrid>()
     fun findGridById(gridID: UUID): MEWirelessConnectionGrid? = MEWirelessGrids.firstOrNull { it.gridID == gridID }
-    fun findGridByPlayerId(playerId: UUID): List<MEWirelessConnectionGrid> = MEWirelessGrids.filter { it.gridPermissionHolder.contains(playerId) }
+    fun findGridByPlayerId(playerId: UUID): List<MEWirelessConnectionGrid> = MEWirelessGrids.filter {
+        TeamUtil.getTeamUUID(playerId) == TeamUtil.getTeamUUID(it.gridOwnerUUid)
+    }
+    fun getGridCount(): Int = MEWirelessGrids.size
 
     fun createNewGrid(gridName: String, playerId: UUID): MEWirelessConnectionGrid? {
+        if (gridName.isEmpty()) return null
         if (MEWirelessGrids.any { it.gridName == gridName }) return null
         val newGrid = MEWirelessConnectionGrid(gridName, playerId)
         MEWirelessGrids.add(newGrid)
+        setDirty()
         return newGrid
     }
     fun removeGrid(grid: MEWirelessConnectionGrid) {
-        MEWirelessGrids.remove(grid)
+        grid.connectionPool.forEach { it.clearSavedGridData() }
         grid.destroyAllConnection()
+        MEWirelessGrids.remove(grid)
+        setDirty()
     }
     override fun save(tag: CompoundTag): CompoundTag {
         tag.put(NBT_GRID, ListTag().apply { MEWirelessGrids.forEach { add(it.serializeNBT()) } })
         return tag
     }
     fun load(tag: CompoundTag): MEWirelessSavedData {
-        tag.getList(NBT_GRID, 10).forEach { MEWirelessGrids.add(MEWirelessConnectionGrid("DEFAULT",UUID.randomUUID()).apply { deserializeNBT(it as CompoundTag) }) }
+        tag.getList(NBT_GRID, 10).forEach { compoundTag ->
+            val element = MEWirelessConnectionGrid.deserializeNBT(compoundTag as CompoundTag) ?: return@forEach
+            if (MEWirelessGrids.none { it.gridID == element.gridID }) {
+                MEWirelessGrids.add(element)
+            }
+        }
         return this
     }
 
     // 每个无线机器网络，处理无线连机的加入，退出，连接，刷新
-    class MEWirelessConnectionGrid(val gridName_: String,playerId: UUID) {
+    class MEWirelessConnectionGrid(var gridName: String, var gridOwnerUUid: UUID, var gridID: UUID = UUID.nameUUIDFromBytes(gridName.toByteArray())) {
         // ////////////////////////////////
         // ****** 网络元数据(保存在存档) ******//
         // //////////////////////////////
-        var gridName: String = gridName_
-        var gridID: UUID = UUID.nameUUIDFromBytes(gridName.toByteArray())
-        var gridOwnerUUid: UUID = playerId // 拥有修改网络权限的权限
-        var gridPermissionHolder: MutableList<UUID> = mutableListOf() // 可以发现此网络的UUID或者团队
-            get() = field.apply {
-                if (!field.contains(gridOwnerUUid)) field.add(gridOwnerUUid)
-                ServerLifecycleHooks.getCurrentServer().playerList.players.forEach { field.add(it.uuid) }
-            }.distinct().toMutableList()
-
-        fun serializeNBT(): CompoundTag = CompoundTag().apply {
-            putUUID("gridID", gridID)
-            putString("gridName", gridName)
-            putUUID("gridOwnerUUid", gridOwnerUUid)
-            put(
-                "gridPermissionHolder",
-                ListTag().apply {
-                    gridPermissionHolder.forEach { add((StringTag.valueOf(it.toString()))) }
-                },
-            )
-        }
-
-        fun deserializeNBT(p0: CompoundTag) {
-            p0.apply {
-                gridID = getUUID("gridID")
-                gridName = getString("gridName")
-                gridOwnerUUid = getUUID("gridOwnerUUid")
-                gridPermissionHolder = mutableListOf()
-                getList("gridPermissionHolder", 8).forEach {
-                    gridPermissionHolder.add(UUID.fromString(it.asString))
-                }
-                gridPermissionHolder.distinct()
+        companion object {
+            val CODEC: Codec<MEWirelessConnectionGrid> = RecordCodecBuilder.create { builder ->
+                builder.group(
+                    Codec.STRING.fieldOf("gridName").forGetter { it.gridName },
+                    UUIDUtil.CODEC.fieldOf("gridOwnerUUid").forGetter { it.gridOwnerUUid },
+                    UUIDUtil.CODEC.fieldOf("gridID").forGetter { it.gridID },
+                ).apply(builder, ::MEWirelessConnectionGrid)
             }
+            fun deserializeNBT(p0: CompoundTag): MEWirelessConnectionGrid? = CODEC.parse(NbtOps.INSTANCE, p0)
+                .resultOrPartial { it -> GTOCore.LOGGER.error(it) }
+                .orElse(null)
         }
+        fun serializeNBT(): CompoundTag = CODEC.encodeStart(NbtOps.INSTANCE, this).getOrThrow(false) { GTOCore.LOGGER.error("MEWirelessConnectionGrid serializeNBT error") } as CompoundTag
 
         // ////////////////////////////////
         // ****** 运行时数据(不保存，由机器保存连接数据，并在加载的时候注册) ******//
--- src/main/java/com/gtocore/integration/ae/MEWirelessConnectionMachine.kt
+++ src/main/java/com/gtocore/integration/ae/MEWirelessConnectionMachine.kt
@@ -5,6 +5,7 @@ import com.gtocore.common.saved.MEWirelessSavedData
 
 import net.minecraft.core.Direction
 import net.minecraft.network.chat.Component
+import net.minecraft.world.entity.player.Player
 
 import appeng.me.ManagedGridNode
 import com.gregtechceu.gtceu.api.machine.IMachineBlockEntity
@@ -15,11 +16,11 @@ import com.gregtechceu.gtceu.integration.ae2.machine.trait.GridNodeHolder
 import com.gtolib.api.gui.ktflexible.blank
 import com.gtolib.api.gui.ktflexible.button
 import com.gtolib.api.gui.ktflexible.field
-import com.gtolib.api.gui.ktflexible.root
 import com.gtolib.api.gui.ktflexible.rootFresh
 import com.lowdragmc.lowdraglib.gui.modular.ModularUI
 import com.lowdragmc.lowdraglib.syncdata.annotation.Persisted
-import net.minecraft.world.entity.player.Player
+import com.lowdragmc.lowdraglib.syncdata.field.ManagedFieldHolder
+import kotlinx.coroutines.Runnable
 
 import java.util.*
 
@@ -27,6 +28,12 @@ class MEWirelessConnectionMachine(holder: IMachineBlockEntity) :
     MetaMachine(holder),
     IGridConnectedMachine,
     IFancyUIMachine {
+    companion object {
+        @JvmStatic
+        val holder = ManagedFieldHolder(MEWirelessConnectionMachine::class.java, MANAGED_FIELD_HOLDER)
+    }
+    override fun getFieldHolder() = Companion.holder
+
     // ////////////////////////////////
     // ****** Grid Initialization ******//
     // //////////////////////////////
@@ -39,7 +46,7 @@ class MEWirelessConnectionMachine(holder: IMachineBlockEntity) :
         super.onLoad()
         val serverInitializer by lazy {
             println("MEWirelessConnectionMachine initialized: $pos at ${level?.dimension()?.location()} in Remote $isRemote")
-            MEWirelessSavedData.findGridById(savedGird ?: UUID.randomUUID())?.let {
+            MEWirelessSavedData.findGridById(savedGirdUUID ?: UUID.randomUUID())?.let {
                 joinGrid(it)
             }
         }
@@ -48,6 +55,14 @@ class MEWirelessConnectionMachine(holder: IMachineBlockEntity) :
                 if (!isRemote) serverInitializer
             }
         }
+    }
+
+    override fun clientTick() {
+        if (offsetTimer > freshTick && freshShould) {
+            freshShould = false
+            freshRun.run()
+        }
+        super.clientTick()
     }
 
     override fun isRemote() = super<MetaMachine>.isRemote
@@ -67,29 +82,46 @@ class MEWirelessConnectionMachine(holder: IMachineBlockEntity) :
     // ****** LOGIC ******//
     // //////////////////////////////
     @Persisted
-    var savedGird: UUID? = null
+    var savedGirdUUID: UUID? = null
     var selectedGrid: MEWirelessSavedData.MEWirelessConnectionGrid? = null
     fun joinGrid(grid: MEWirelessSavedData.MEWirelessConnectionGrid) {
         selectedGrid = grid
+        savedGirdUUID = grid.gridID
         grid.addToConnectionPool(this)
     }
     fun leaveGrid(grid: MEWirelessSavedData.MEWirelessConnectionGrid) {
         selectedGrid = null
         grid.removeFromConnectionPool(this)
     }
+    fun clearSavedGridData() {
+        savedGirdUUID = null
+        selectedGrid = null
+    }
     fun changeToGrid(grid: MEWirelessSavedData.MEWirelessConnectionGrid) {
-        selectedGrid?.let { leaveGrid(it) }
+        selectedGrid?.let {
+            leaveGrid(it)
+            clearSavedGridData()
+        }
         joinGrid(grid)
     }
 
     // ////////////////////////////////
     // ****** UI ******//
     // //////////////////////////////
+    @Persisted
     var nameWillAdd: String = ""
     lateinit var playerUUID: UUID
     override fun createUI(entityPlayer: Player): ModularUI {
         playerUUID = entityPlayer.uuid
         return super.createUI(entityPlayer)
+    }
+    var freshRun: Runnable = Runnable {}
+    var freshShould = false
+    var freshTick = 0L
+    fun triggerClientFresh() {
+        if (!isRemote)return
+        freshShould = true
+        freshTick = offsetTimer + 20
     }
 
     override fun createUIWidget() = rootFresh(176, 166) {
@@ -101,30 +133,40 @@ class MEWirelessConnectionMachine(holder: IMachineBlockEntity) :
                 hBox(height = 16, { spacing = 4 }) {
                     field(width = 60, getter = { nameWillAdd }, setter = { nameWillAdd = it })
                     button(text = { "创建新的无线网络" }, width = this@vBox.availableWidth - 60 - 8) {
-                        if (isRemote) return@button
-                        MEWirelessSavedData.createNewGrid(nameWillAdd, playerUUID)?.let {
-                            MEWirelessSavedData.MEWirelessGrids.add(it)
-                            changeToGrid(it)
+                        if (!isRemote) {
+                            MEWirelessSavedData.createNewGrid(nameWillAdd, playerUUID)?.let {
+                                changeToGrid(it)
+                                freshRun.run()
+                            }
                         }
-
+                        triggerClientFresh()
                     }
                 }
-                vBox(width = availableWidth, { spacing = 2 })a@{
-                    MEWirelessSavedData.findGridByPlayerId(ownerUUID ?: UUID.randomUUID()).forEach { grid ->
-                        hBox(height = 16, { spacing = 4 }) {
-                            button(text = { "切换到 ${grid.gridName}" }, width = this@a.availableWidth - 48 - 8) {
-                                if (isRemote) return@button
-                                changeToGrid(grid)
+                textBlock(maxWidth = availableWidth - 4, textSupplier = { Component.literal("全球可用无线网络 : ${MEWirelessSavedData.findGridByPlayerId(playerUUID).size} / ${MEWirelessSavedData.getGridCount()}") })
+                textBlock(maxWidth = availableWidth - 4, textSupplier = { Component.literal("你的无线网络 : ") })
+                vScroll(width = availableWidth, height = 176 - 4 - 20 - 36 - 16, { spacing = 2 })a@{
+                    MEWirelessSavedData.findGridByPlayerId(playerUUID).forEach { grid ->
+                        hBox(height = 14, { spacing = 4 }) {
+                            button(height = 14, text = { "切换到 ${grid.gridName}" }, width = this@a.availableWidth - 48 - 8) {
+                                if (!isRemote)changeToGrid(grid)
                             }
-                            button(text = { "删除" }, width = 48) {
-                                if (isRemote) return@button
-                                leaveGrid(grid)
-                                MEWirelessSavedData.removeGrid(grid)
+                            if (playerUUID == grid.gridOwnerUUid) {
+                                button(height = 14, text = { "删除" }, width = 48) {
+                                    if (!isRemote) {
+                                        MEWirelessSavedData.removeGrid(grid)
+                                        freshRun.run()
+                                    }
+                                    triggerClientFresh()
+                                }
+                            } else {
+                                hBox(height = 14, alwaysVerticalCenter = true) {
+                                    textBlock(maxWidth = 48, textSupplier = { Component.literal("你无权删除") })
+                                }
                             }
                         }
                     }
                 }
             }
         }
-    }
+    }.also { freshRun = Runnable { it.fresh() } }
 }
