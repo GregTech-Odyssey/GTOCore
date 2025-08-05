0b68a6e17da831be9118bd7b2d40b876934e1e04
Hophenby <1094009969@qq.com>
2025-08-05 13:22:28 +0800

    使无尽图腾不再能被修复，并修改其耐久的计算方式，以弥补037玩家过早拿到强力奖励无尽图腾的不平衡

--- src/main/java/com/gtocore/mixin/mc/UnbreakingMixin.java
+++ src/main/java/com/gtocore/mixin/mc/UnbreakingMixin.java
@@ -13,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(DigDurabilityEnchantment.class)
 public class UnbreakingMixin {
 
-    @Inject(method = "shouldIgnoreDurabilityDrop", at = @At("HEAD"), cancellable = true, remap = false)
+    @Inject(method = "shouldIgnoreDurabilityDrop", at = @At("HEAD"), cancellable = true)
     private static void gto$shouldIgnoreDurabilityDrop(ItemStack stack, int level, RandomSource random, CallbackInfoReturnable<Boolean> cir) {
         if (stack.getItem() instanceof InfinityTotemItem && random.nextFloat() < 0.6F) {
             cir.setReturnValue(false);
