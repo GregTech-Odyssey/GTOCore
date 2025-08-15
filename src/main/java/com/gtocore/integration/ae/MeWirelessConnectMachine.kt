f6e1045044b61433209bc5ed7fe01862404869be
Hophenby <1094009969@qq.com>
2025-08-15 17:17:23 +0800

    新增ME无线连接机渲染器，更新相关翻译和配置 lib的emi插件也可以换一下https://github.com/GregTech-Odyssey/ae2-emi-crafting-forge

--- src/main/java/com/gtocore/integration/ae/MeWirelessConnectMachine.kt
+++ src/main/java/com/gtocore/integration/ae/MeWirelessConnectMachine.kt
@@ -38,7 +38,6 @@ class MeWirelessConnectMachine(holder: MetaMachineBlockEntity) :
         val syncManager = SyncManagedFieldHolder(MeWirelessConnectMachine::class.java)
     }
     val gridHolder = GridNodeHolder(this)
-    @DescSynced
     var isGridOnline: Boolean = false
     var isClientInit = false
 
