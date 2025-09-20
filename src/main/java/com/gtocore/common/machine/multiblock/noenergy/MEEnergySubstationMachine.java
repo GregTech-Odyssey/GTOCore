c41d0de5afc34aa939413d0ddfb452a5949eb84d
gjmhmm8 <gjmhmm123456@163.com>
2025-09-20 18:45:26 +0800

    fix

--- src/main/java/com/gtocore/common/machine/multiblock/noenergy/MEEnergySubstationMachine.java
+++ src/main/java/com/gtocore/common/machine/multiblock/noenergy/MEEnergySubstationMachine.java
@@ -7,57 +7,47 @@ import com.gregtechceu.gtceu.api.capability.recipe.IO;
 import com.gregtechceu.gtceu.api.machine.feature.multiblock.IMultiPart;
 import com.gregtechceu.gtceu.api.misc.EnergyContainerList;
 import com.gtocore.common.machine.multiblock.part.ae.MEEnergyAccessPartMachine;
-import com.gtolib.api.machine.feature.multiblock.IMultiblockTraitHolder;
-import com.gtolib.api.machine.feature.multiblock.ITierCasingMachine;
-import com.gtolib.api.machine.multiblock.NoRecipeLogicMultiblockMachine;
-import com.gtolib.api.machine.trait.TierCasingTrait;
-import it.unimi.dsi.fastutil.objects.Object2IntMap;
+import com.gtolib.api.machine.multiblock.TierCasingMultiblockMachine;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
 import static com.gtolib.api.GTOValues.GLASS_TIER;
 
-public class MEEnergySubstationMachine extends NoRecipeLogicMultiblockMachine implements ITierCasingMachine, IMultiblockTraitHolder {
-    private final TierCasingTrait tierCasingTrait;
-    private EnergyContainerList inputHatches=null;
+public class MEEnergySubstationMachine extends TierCasingMultiblockMachine {
+    private EnergyContainerList inputHatches = null;
 
     public MEEnergySubstationMachine(MetaMachineBlockEntity holder) {
-        super(holder);
-        tierCasingTrait = new TierCasingTrait(this, GLASS_TIER);
+        super(holder, GLASS_TIER);
     }
 
     @Override
     public void onStructureFormed() {
         super.onStructureFormed();
         List<IEnergyContainer> inputs = new ObjectArrayList<>();
-        for(IMultiPart part : this.getParts()) {
+        for (IMultiPart part : this.getParts()) {
             for (var handlerList : part.getRecipeHandlers()) {
-                    var containers = handlerList.getCapability(EURecipeCapability.CAP).stream().filter(IEnergyContainer.class::isInstance).map(IEnergyContainer.class::cast).toList();
-                    if (handlerList.getHandlerIO() == IO.IN) {
-                        inputs.addAll(containers);
-                    }
+                var containers = handlerList.getCapability(EURecipeCapability.CAP).stream().filter(IEnergyContainer.class::isInstance).map(IEnergyContainer.class::cast).toList();
+                if (handlerList.getHandlerIO() == IO.IN) {
+                    inputs.addAll(containers);
                 }
+            }
         }
         this.inputHatches = new EnergyContainerList(inputs);
-        for(IMultiPart part : this.getParts()){
-            if(part instanceof MEEnergyAccessPartMachine me_part)me_part.onFormatted(this);
+        for (IMultiPart part : this.getParts()) {
+            if (part instanceof MEEnergyAccessPartMachine me_part) me_part.onFormatted(this);
         }
     }
 
     @Override
     public void onStructureInvalid() {
         super.onStructureInvalid();
-        this.inputHatches=null;
-    }
-
-    @Override
-    public Object2IntMap<String> getCasingTiers() {
-        return tierCasingTrait.getCasingTiers();
+        this.inputHatches = null;
     }
 
 
-    public EnergyContainerList getEnergyContainer() {
+    public @NotNull EnergyContainerList getEnergyContainer() {
         return inputHatches;
     }
 }
