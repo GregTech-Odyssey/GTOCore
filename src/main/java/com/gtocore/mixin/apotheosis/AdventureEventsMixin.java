f333a507ba8d170b3e1a9ef886a8184c56d24a1c
wanggugu <87158048+wanggugu197@users.noreply.github.com>
2025-09-24 00:04:38 +0800

    大型炼金装置翻新 魔力输出仓可以输出到魔源罐

--- src/main/java/com/gtocore/mixin/apotheosis/AdventureEventsMixin.java
+++ src/main/java/com/gtocore/mixin/apotheosis/AdventureEventsMixin.java
@@ -1,10 +1,5 @@
 package com.gtocore.mixin.apotheosis;
 
-import dev.shadowsoffire.apotheosis.adventure.AdventureConfig;
-import dev.shadowsoffire.apotheosis.adventure.AdventureEvents;
-import dev.shadowsoffire.apotheosis.adventure.compat.GameStagesCompat;
-import dev.shadowsoffire.apotheosis.adventure.socket.gem.GemRegistry;
-import dev.shadowsoffire.placebo.reload.WeightedDynamicRegistry;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
@@ -13,6 +8,12 @@ import net.minecraft.world.entity.monster.Monster;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+import dev.shadowsoffire.apotheosis.adventure.AdventureConfig;
+import dev.shadowsoffire.apotheosis.adventure.AdventureEvents;
+import dev.shadowsoffire.apotheosis.adventure.compat.GameStagesCompat;
+import dev.shadowsoffire.apotheosis.adventure.socket.gem.GemRegistry;
+import dev.shadowsoffire.placebo.reload.WeightedDynamicRegistry;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 
