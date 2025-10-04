ddd56fbdf17cc6b9a9a678f225c6d7c483631f66
xinxinsuried <xinxinsuried@gmail.com>
2025-10-04 16:45:33 +0800

    加片片支持

--- src/main/java/com/gtocore/mixin/ae2/QuartzCuttingKnifeItemMixin.java
+++ src/main/java/com/gtocore/mixin/ae2/QuartzCuttingKnifeItemMixin.java
@@ -1,6 +1,5 @@
 package com.gtocore.mixin.ae2;
 
-import appeng.items.tools.quartz.QuartzCuttingKnifeItem;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
@@ -8,9 +7,15 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+
+import appeng.api.parts.IPart;
+import appeng.blockentity.networking.CableBusBlockEntity;
+import appeng.items.tools.quartz.QuartzCuttingKnifeItem;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -29,8 +34,21 @@ public class QuartzCuttingKnifeItemMixin {
             if (level.isClientSide) {
                 BlockPos pos = context.getClickedPos();
                 BlockState state = level.getBlockState(pos);
-                if (!state.isAir()) {
-                    Component blockNameComponent = state.getBlock().getName();
+                BlockEntity tile = level.getBlockEntity(pos);
+                Component blockNameComponent = null;
+
+                if (tile instanceof CableBusBlockEntity cable) {
+                    Vec3 hitVec = context.getClickLocation();
+                    Vec3 hitInBlock = new Vec3(hitVec.x - pos.getX(), hitVec.y - pos.getY(), hitVec.z - pos.getZ());
+                    IPart part = cable.getCableBus().selectPartLocal(hitInBlock).part;
+                    if (part != null) {
+                        blockNameComponent = part.getPartItem().asItem().getName(part.getPartItem().asItem().getDefaultInstance());
+                    }
+                }
+                if (blockNameComponent == null && !state.isAir()) {
+                    blockNameComponent = state.getBlock().getName();
+                }
+                if (blockNameComponent != null) {
                     gtocore$setClipboard(blockNameComponent);
                     player.displayClientMessage(blockNameComponent, false);
                 }
