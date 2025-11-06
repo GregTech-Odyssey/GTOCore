c94257212a4f975e23556dc8155199a8513b0725
Hophenby <66473090+Hophenby@users.noreply.github.com>
2025-11-06 17:11:53 +0800

    2合1终端支持显示元件 (#276)

--- src/main/java/com/gtocore/common/machine/multiblock/electric/miner/DigitalMinerLogic.java
+++ src/main/java/com/gtocore/common/machine/multiblock/electric/miner/DigitalMinerLogic.java
@@ -238,7 +238,7 @@ public class DigitalMinerLogic extends CustomRecipeLogic {
                         BlockState state = chunkCache.getBlockState(blockPos);
                         if (!isInMultiblock(blockPos) &&
                                 state.getBlock() != Blocks.AIR &&
-                                (!state.hasBlockEntity() || (itemFilter != null && itemFilter.test(((IItem) state.getBlock().asItem()).gtolib$getReadOnlyStack()))) &&
+                                ((itemFilter != null && itemFilter.test(((IItem) state.getBlock().asItem()).gtolib$getReadOnlyStack()))) &&
                                 state.getBlock().defaultDestroyTime() >= 0) {
                             if (state.getBlock() instanceof LiquidBlock liq && fluidMode != DigitalMiner.FluidMode.Ignore && itemFilter == null) {
                                 if (fluidFilter == null || fluidFilter.test(new FluidStack(liq.getFluidState(state).getType(), 1))) {
