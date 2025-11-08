0288e12e0d24a0938832fa1e192aee2a34b7e10e
gjmhmm8 <73775469+gjmhmm8@users.noreply.github.com>
2025-11-08 21:14:57 +0800

    可选MOD配方 (#280)
    Co-authored-by: nutant <65939836+nutant233@users.noreply.github.com>

--- src/main/java/com/gtocore/data/recipe/classified/Vanilla.java
+++ src/main/java/com/gtocore/data/recipe/classified/Vanilla.java
@@ -889,7 +889,7 @@ final class Vanilla {
                 "ABA",
                 "BCB",
                 "ABA",
-                'A', RegistriesUtils.getItemStack("gtceu:bronze_crate"), 'B', Items.CHEST.asItem(), 'C', MultiBlockG.MULTIBLOCK_CRATE.asItem());
+                'A', GTMachines.STEEL_CRATE.asItem(), 'B', Items.CHEST.asItem(), 'C', MultiBlockG.MULTIBLOCK_CRATE.asItem());
         VanillaRecipeHelper.addShapedRecipe(GTOCore.id("structure_detect"), GTOItems.STRUCTURE_DETECT.asItem(),
                 " A ",
                 "ABA",
