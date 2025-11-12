cae27a7bc349a8b4ae3052cac4cdad8113aad937
wanggugu <87158048+wanggugu197@users.noreply.github.com>
2025-11-12 15:10:16 +0800

    fix

--- src/main/java/com/gtocore/api/gui/GTOGuiTextures.java
+++ src/main/java/com/gtocore/api/gui/GTOGuiTextures.java
@@ -10,24 +10,28 @@ public final class GTOGuiTextures {
 
     public static final ResourceTexture PARALLEL_CONFIG = new ResourceTexture(GTCEu.id("textures/gui/icon/io_config/cover_settings.png"));
 
-    public static final ResourceTexture REFRESH = new ResourceTexture(GTOCore.id("textures/gui/base/refresh.png"));
-    public static final ResourceTexture DELETE = new ResourceTexture(GTOCore.id("textures/gui/base/delete"));
+    public static final ResourceTexture REFRESH = getTexture("base/refresh");
+    public static final ResourceTexture DELETE = getTexture("base/delete");
     public static final ResourceTexture[] VILLAGER_RECIPE_SLOTS = {
-            new ResourceTexture(GTOCore.id("textures/gui/base/villager_recipe_slot_1.png")),
-            new ResourceTexture(GTOCore.id("textures/gui/base/villager_recipe_slot_2.png")),
-            new ResourceTexture(GTOCore.id("textures/gui/base/villager_recipe_slot_3.png")) };
+            getTexture("base/villager_recipe_slot_0"),
+            getTexture("base/villager_recipe_slot_1"),
+            getTexture("base/villager_recipe_slot_2") };
 
-    public static final ResourceTexture PROGRESS_BAR_DATA_GENERATE_BASE = new ResourceTexture(GTOCore.id("textures/gui/progress_bar/progress_bar_data_generate_base.png"));
-    public static final ResourceTexture PROGRESS_BAR_RESEARCH_BASE = new ResourceTexture(GTOCore.id("textures/gui/progress_bar/progress_bar_research_base.png"));
-    public static final ResourceTexture CONDENSE_FROM_FLUID = new ResourceTexture(GTOCore.id("textures/gui/progress_bar/condense_from_fluid.png"));
-    public static final ResourceTexture CONDENSE_FROM_PLASMA = new ResourceTexture(GTOCore.id("textures/gui/progress_bar/condense_from_plasma.png"));
-    public static final ResourceTexture CONDENSE_FROM_MOLTEN = new ResourceTexture(GTOCore.id("textures/gui/progress_bar/condense_from_molten.png"));
-    public static final ResourceTexture PROGRESS_BAR_MINING_MODULE = new ResourceTexture(GTOCore.id("textures/gui/progress_bar/progress_bar_mining_module"));
-    public static final ResourceTexture PROGRESS_BAR_DRILLING_MODULE = new ResourceTexture(GTOCore.id("textures/gui/progress_bar/progress_bar_drilling_module"));
+    public static final ResourceTexture PROGRESS_BAR_DATA_GENERATE_BASE = getTexture("progress_bar/progress_bar_data_generate_base");
+    public static final ResourceTexture PROGRESS_BAR_RESEARCH_BASE = getTexture("progress_bar/progress_bar_research_base");
+    public static final ResourceTexture CONDENSE_FROM_FLUID = getTexture("progress_bar/condense_from_fluid");
+    public static final ResourceTexture CONDENSE_FROM_PLASMA = getTexture("progress_bar/condense_from_plasma");
+    public static final ResourceTexture CONDENSE_FROM_MOLTEN = getTexture("progress_bar/condense_from_molten");
+    public static final ResourceTexture PROGRESS_BAR_MINING_MODULE = getTexture("progress_bar/progress_bar_mining_module");
+    public static final ResourceTexture PROGRESS_BAR_DRILLING_MODULE = getTexture("progress_bar/progress_bar_drilling_module");
 
-    public static final ResourceTexture DATA_CRYSTAL_OVERLAY = new ResourceTexture(GTOCore.id("textures/gui/overlay/data_crystal_overlay"));
-    public static final ResourceTexture PLANET_TELEPORT = new ResourceTexture(GTOCore.id("textures/gui/overlay/planet_teleport"));
-    public static final ResourceTexture HIGH_SPEED_MODE = new ResourceTexture(GTOCore.id("textures/gui/overlay/high_speed_mode"));
-    public static final ResourceTexture OVERCLOCK_CONFIG = new ResourceTexture(GTOCore.id("textures/gui/overlay/overclock_config"));
-    public static final ResourceTexture STRUCTURE_CHECK = new ResourceTexture(GTOCore.id("textures/gui/overlay/structure_check"));
+    public static final ResourceTexture DATA_CRYSTAL_OVERLAY = getTexture("overlay/data_crystal_overlay");
+    public static final ResourceTexture PLANET_TELEPORT = getTexture("overlay/planet_teleport");
+    public static final ResourceTexture HIGH_SPEED_MODE = getTexture("overlay/high_speed_mode");
+    public static final ResourceTexture OVERCLOCK_CONFIG = getTexture("overlay/overclock_config");
+    public static final ResourceTexture STRUCTURE_CHECK = getTexture("overlay/structure_check");
+
+    private static ResourceTexture getTexture(String string) {
+        return new ResourceTexture(GTOCore.id("textures/gui/" + string + ".png"));
+    }
 }
