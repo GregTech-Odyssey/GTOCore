aad964e9a11e21535e5079cfe6b895a33b7d3790
xinxinsuried <76897393+xinxinsuried@users.noreply.github.com>
2026-01-02 21:22:03 +0800

    加鱼 (#342)
    如你所见，加了四条可以吃的鱼，还有五种剁椒鱼头，会在地牢生成

--- src/main/java/com/gtocore/common/data/GTOItems.java
+++ src/main/java/com/gtocore/common/data/GTOItems.java
@@ -1280,4 +1280,37 @@ public final class GTOItems {
     public static final ItemEntry<Item> STOPGAP_MEASURES = item("stopgap_measures", "权宜之计")
             .toolTips(ComponentBuilder.create().addLines("§7在写了~~§r", "§7On working~~§r").build().getArray())
             .register();
+
+    public static final ItemEntry<RandomPositiveFoodItem> RANDOM_POSITIVE_FOOD_1 = item("delicious_food_1", "好吃的鱼 I", p -> new RandomPositiveFoodItem(p, 4, 0.6F))
+            .model((a, b) -> b.generated(a, GTOCore.id("item/food/fish/fish1")))
+            .register();
+
+    public static final ItemEntry<RandomPositiveFoodItem> RANDOM_POSITIVE_FOOD_2 = item("delicious_food_2", "好吃的鱼 II", p -> new RandomPositiveFoodItem(p, 6, 0.7F))
+            .model((a, b) -> b.generated(a, GTOCore.id("item/food/fish/fish2")))
+            .register();
+
+    public static final ItemEntry<RandomPositiveFoodItem> RANDOM_POSITIVE_FOOD_3 = item("delicious_food_3", "好吃的鱼 III", p -> new RandomPositiveFoodItem(p, 8, 0.8F))
+            .model((a, b) -> b.generated(a, GTOCore.id("item/food/fish/fish3")))
+            .register();
+
+    public static final ItemEntry<RandomPositiveFoodItem> RANDOM_POSITIVE_FOOD_4 = item("delicious_food_4", "好吃的鱼 IV", p -> new RandomPositiveFoodItem(p, 10, 0.9F))
+            .model((a, b) -> b.generated(a, GTOCore.id("item/food/fish/fish4")))
+            .register();
+
+    public static final ItemEntry<RandomPositiveFoodItem> CHOPPER_POPPER_FISH_HEAD_1 = item("chopper_popper_fish_head_1", "剁椒鱼头 I", p -> new RandomPositiveFoodItem(p, 4, 0.6F))
+            .model((a, b) -> b.generated(a, GTOCore.id("item/food/fishhead/fish_head0")))
+            .register();
+
+    public static final ItemEntry<RandomPositiveFoodItem> CHOPPER_POPPER_FISH_HEAD_2 = item("chopper_popper_fish_head_2", "剁椒鱼头 II", p -> new RandomPositiveFoodItem(p, 6, 0.7F))
+            .model((a, b) -> b.generated(a, GTOCore.id("item/food/fishhead/fish_head1")))
+            .register();
+    public static final ItemEntry<RandomPositiveFoodItem> CHOPPER_POPPER_FISH_HEAD_3 = item("chopper_popper_fish_head_3", "剁椒鱼头 III", p -> new RandomPositiveFoodItem(p, 8, 0.8F))
+            .model((a, b) -> b.generated(a, GTOCore.id("item/food/fishhead/fish_head2")))
+            .register();
+    public static final ItemEntry<RandomPositiveFoodItem> CHOPPER_POPPER_FISH_HEAD_4 = item("chopper_popper_fish_head_4", "剁椒鱼头 IV", p -> new RandomPositiveFoodItem(p, 10, 0.9F))
+            .model((a, b) -> b.generated(a, GTOCore.id("item/food/fishhead/fish_head3")))
+            .register();
+    public static final ItemEntry<RandomPositiveFoodItem> CHOPPER_POPPER_FISH_HEAD_5 = item("chopper_popper_fish_head_5", "剁椒鱼头 V", p -> new RandomPositiveFoodItem(p, 10, 1F))
+            .model((a, b) -> b.generated(a, GTOCore.id("item/food/fishhead/fish_head4")))
+            .register();
 }
