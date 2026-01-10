ada44184098afd215730ee657511697b40a6d10e
MuuuShin <1248440677@qq.com>
2026-01-11 05:58:47 +0800

    fix: 真空干燥的线圈并行显示

--- src/main/java/com/gtocore/common/data/machines/MultiBlockA.java
+++ src/main/java/com/gtocore/common/data/machines/MultiBlockA.java
@@ -1688,7 +1688,9 @@ public final class MultiBlockA {
             .tooltipsKey("gtocore.machine.recipe.run", Component.translatable("gtceu.vacuum_drying"))
             .tooltipsKey("gtceu.machine.electric_blast_furnace.tooltip.0")
             .tooltipsKey("gtceu.machine.electric_blast_furnace.tooltip.1")
-            .coilParallelTooltips()
+            .tooltipsKey("gtocore.machine.recipe.run", Component.translatable("gtceu.dehydrator"))
+            .tooltipsText("线圈温度每高出900K，并行数x2", "For every 900K increase in coil temperature, the parallel number doubles")
+            .tooltipsText("§7公式 : 2^(向下取整(温度 / 900))", "§7Formula: 2^(Round down(temperature / 900))")
             .disabledCombined()
             .recipeModifier((m, r) -> {
                 if (m instanceof CoilCustomParallelMultiblockMachine machine) {
