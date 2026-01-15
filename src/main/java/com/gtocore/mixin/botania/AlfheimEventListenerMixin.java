d4c62f433fb06c13527d00fe1e850821718cab49
Hophenby <1094009969@qq.com>
2026-01-15 14:54:32 +0800

    spotless

--- src/main/java/com/gtocore/mixin/botania/AlfheimEventListenerMixin.java
+++ src/main/java/com/gtocore/mixin/botania/AlfheimEventListenerMixin.java
@@ -1,9 +1,9 @@
 package com.gtocore.mixin.botania;
 
 import com.gtolib.api.data.Dimension;
+
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
