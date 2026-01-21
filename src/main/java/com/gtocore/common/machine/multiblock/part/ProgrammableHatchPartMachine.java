6ef987e2a8b61ecd4099def10f63b3539e97a1e0
FYWinds <i@windis.cn>
2026-01-21 11:35:41 -0500

    fix: 虚拟物品

--- src/main/java/com/gtocore/common/machine/multiblock/part/ProgrammableHatchPartMachine.java
+++ src/main/java/com/gtocore/common/machine/multiblock/part/ProgrammableHatchPartMachine.java
@@ -142,7 +142,7 @@ public class ProgrammableHatchPartMachine extends DualHatchPartMachine implement
             @NotNull
             @Override
             public ItemStack insertItem(int slot, @NotNull ItemStack stack, boolean simulate) {
-                if (machine.isProgrammable() && stack.is(CustomItems.VIRTUAL_ITEM_PROVIDER.get())) {
+                if (machine.isProgrammable() && stack.hasTag() && stack.is(CustomItems.VIRTUAL_ITEM_PROVIDER.get())) {
                     setStackInSlot(slot, VirtualItemProviderBehavior.getVirtualItem(stack));
                     return ItemStack.EMPTY;
                 }
