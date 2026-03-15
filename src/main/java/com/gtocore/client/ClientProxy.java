2bf4b8ac06f15a08b2eb33d2ddbcef209e9f2db8
Hophenby <1094009969@qq.com>
2026-03-15 18:50:37 +0800

    修复服务器崩溃

--- src/main/java/com/gtocore/client/ClientProxy.java
+++ src/main/java/com/gtocore/client/ClientProxy.java
@@ -18,6 +18,7 @@ import com.gtocore.integration.ae.PatternContentAccessTerminalPart;
 import com.gtocore.integration.ae.PatternContentAccessTerminalScreen;
 
 import com.gtolib.GTOCore;
+import com.gtolib.api.ae2.gui.GTOButtonAppearance;
 import com.gtolib.api.ae2.me2in1.Me2in1Menu;
 import com.gtolib.api.ae2.me2in1.Me2in1Screen;
 import com.gtolib.api.ae2.me2in1.Me2in1TerminalPart;
@@ -52,6 +53,7 @@ import net.minecraftforge.registries.ForgeRegistries;
 import appeng.api.client.AEKeyRendering;
 import appeng.api.parts.PartModels;
 import appeng.api.util.AEColor;
+import appeng.client.gui.widgets.SettingToggleButton;
 import appeng.client.render.StaticItemColor;
 import appeng.init.client.InitScreens;
 
@@ -84,6 +86,7 @@ public final class ClientProxy extends CommonProxy {
         registerAEModels();
         AEKeyRendering.register(TagPrefixKeyType.TYPE, TagPrefixKey.class, new TagPrefixRenderer.AEKeyHandler());
         if (GTCEu.Mods.isShimmerLoaded()) eventBus.addListener(ClientProxy::registerLights);
+        SettingToggleButton.deferAppearanceRegistration(GTOButtonAppearance::registerButtons);
     }
 
     private static void init() {
