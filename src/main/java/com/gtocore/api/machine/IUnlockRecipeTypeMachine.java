e8f5bc2de271f61af10e37a67930c21694ed1016
MuuuShin <97760035+MuuuShin@users.noreply.github.com>
2026-05-18 14:23:19 +0800

    fix: super.getRealRecipe传Null导致的NPE

--- src/main/java/com/gtocore/api/machine/IUnlockRecipeTypeMachine.java
+++ src/main/java/com/gtocore/api/machine/IUnlockRecipeTypeMachine.java
@@ -6,11 +6,15 @@ import com.gtolib.api.recipe.Recipe;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 
+import org.jetbrains.annotations.Nullable;
+
 public interface IUnlockRecipeTypeMachine extends IEnhancedRecipeLogicMachine {
 
     boolean canProcess(Recipe recipe);
 
-    default Recipe validateRecipe(Recipe recipe) {
+    @Nullable
+    default Recipe validateRecipe(@Nullable Recipe recipe) {
+        if (recipe == null) return null;
         if (!canProcess(recipe)) {
             getEnhancedRecipeLogic().gtolib$setIdleReason(getLockedReason()
                     .append(": ")
